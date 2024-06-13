13ddaf474f3c64a44ac26d32ecd23942201b4530
IvanRojas FT47b <exequieliv@outlook.es>
2024-06-13 08:36:41 -0300

    funco ahora?

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,35 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if(!userProgress || !userProgress.activeCourse){
+    redirect("/courses")
+ 
+  }
+
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", ImageSrc: "es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
+};
+
+export default LearnPage;
