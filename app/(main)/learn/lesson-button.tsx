dad8ce4b3d5c6074599cac63350f466da144350d
IvanRojas FT47b <exequieliv@outlook.es>
2024-06-16 15:50:47 -0300

    bbuttons

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -59,24 +59,64 @@ export const LessonButton = ({
           marginTop: isFirst && !isCompleted ? 60 : 24,
         }}
       >
-        {current ? <div className="h-[102px] w-[102px] relative">
-            <div className="absolute -top-6 left-2.5 px-3 py-2.5 border-2 font-bold 
-            uppercase text-green-500 bg-white rounded-lg animate-bounce tracking-wide z-10">
-                Start
-                <div 
+        {current ? (
+          <div className="h-[102px] w-[102px] relative">
+            <div
+              className="absolute -top-6 left-2.5 px-3 py-2.5 border-2 font-bold 
+            uppercase text-green-500 bg-white rounded-lg animate-bounce tracking-wide z-10"
+            >
+              Start
+              <div
                 className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8
                 transform -translate-x-1/2
                 "
-                />
+              />
             </div>
             <CircularProgressbarWithChildren
-            value={Number.isNaN(percentage) ? 0 : percentage}
+              value={Number.isNaN(percentage) ? 0 : percentage}
+              styles={{
+                path: {
+                  stroke: "#4ade80",
+                },
+                trail: {
+                  stroke: "#e5e7eb",
+                },
+              }}
             >
-                
+              <Button
+                variant={locked ? "locked" : "secondary"}
+                className="h-[70px] w-[70px] border-b-8 "
+                size="rounded"
+              >
+                <Icon
+                  className={cn(
+                    "h-10 w-10 ",
+                    locked
+                      ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                      : "fill-primary-foregrund text-primary-foreground",
+                    isCompleted && "fill-none stroke-[4]"
+                  )}
+                />
+              </Button>
             </CircularProgressbarWithChildren>
-            </div> 
-                : 
-                <div>somting</div>}
+          </div>
+        ) : (
+          <Button
+            variant={locked ? "locked" : "secondary"}
+            className="h-[70px] w-[70px] border-b-8 "
+            size="rounded"
+          >
+            <Icon
+              className={cn(
+                "h-10 w-10 ",
+                locked
+                  ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                  : "fill-primary-foregrund text-primary-foreground",
+                isCompleted && "fill-none stroke-[4]"
+              )}
+            />
+          </Button>
+        )}
       </div>
     </Link>
   );
