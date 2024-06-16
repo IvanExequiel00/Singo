80f23e37e584f13de1f08e635860edd41c7d431c
IvanRojas FT47b <exequieliv@outlook.es>
2024-06-16 19:11:01 -0300

    queries

--- db/queries.ts
+++ db/queries.ts
@@ -29,6 +29,7 @@ export const getUnits  = cache(async () =>{
         where: eq(units.courseId, userProgress.activeCourseId),
         with:{
             lessons: {
+                
                 with:{
                     challenges:{
                         with:{
@@ -72,3 +73,94 @@ export const getCoursesById = cache(async(courseId: number) =>{
     return data
 })
  
+
+
+
+export const getCourseProgress = cache(async () =>{
+
+    const {userId} = await auth()
+
+   
+    
+    
+    const userProgress  = await getUserProgress();
+
+ if(!userProgress?.activeCourseId){
+        return null
+    }
+    if (!userId || !userProgress?.activeCourse){
+        return null;
+    }
+
+
+    const unitInActiveCourse = await db.query.units.findMany({
+        orderBy: (units, { asc }) => [asc(units.order)],
+        where: eq(units.courseId, userProgress.activeCourseId), 
+        with: {
+            lessons:{
+                orderBy: (lessons, {asc}) => [asc(lessons.order)],
+                with:{
+                    unit: true,
+                    challenges:{
+                        with:{
+                            challengeProgress:{
+                                where: eq(challengeProgress.userId, userId),
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+ const firstUncompletedLesson = unitInActiveCourse
+.flatMap((unit) => unit.lessons)
+.find((lesson) =>{
+    return lesson.challenges.some((challenge) =>{
+        return !challenge.challengeProgress || challenge.challengeProgress.length === 0;
+    })
+})
+
+return{
+    activeLesson: firstUncompletedLesson,
+    activeLessonId: firstUncompletedLesson?.id
+}
+
+});
+
+export const getLesson = cache(async(id?:number) =>{
+    const {userId} = await auth();
+
+if(!userId){
+    return null
+}
+
+
+    const courseProgress = await getCourseProgress();
+
+    const lessonId = id || courseProgress?.activeLessonId;
+    if(!lessonId){
+        return null
+    }
+
+    const data =  await db.query.lessons.findFirst({
+        where: eq(lessons.id, lessonId),
+        with:{
+            challenges:{
+                orderBy: (challenges, {asc}) =>[asc(challenges.order)],
+                with:{
+                    challengeOptions:true,
+                    challengeProgress:{
+                        where: eq(challengeProgress.userId, userId),
+                    }
+                }
+            }
+        }
+    })
+
+    if(!data || !data.challenges){
+        return null;
+    }
+})
+
+
