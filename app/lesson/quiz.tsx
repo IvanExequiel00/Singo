ff579e8ef28569c146637635b26f176c4ade5b57
IvanRojas FT47b <exequieliv@outlook.es>
2024-06-19 18:54:12 -0300

    correct response

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -43,10 +43,39 @@ export const Quiz = ({
     const challenge = challenges[activeIndex];
     const options = challenge?.challengeOptions ?? [];
 
+    const onNext = () => {
+        setActiveIndex((current) => current + 1);
+    }
+
     const onSelect = (id:number) => {
         if(status !== "none") return;
 
         setSelectedOption(id);
+    }
+
+    const onContinue = () =>{
+        if(!selectedOption) return;
+
+        if(status === "wrong" ){
+            setStatus("none");
+            setSelectedOption(undefined)
+            return;
+        }
+        if(status === "correct" ){
+            onNext();
+            setStatus("none");
+            setSelectedOption(undefined)
+            return;
+        }
+
+        const correctOptions = options.find((option) => option.correct);
+
+        if(correctOptions && correctOptions.id === selectedOption) {
+            console.log("correct options")
+        }else{
+            console.error("Incorrect Option")
+        }
+
     }
 
     const title = challenge.type === "ASSIST" ? "Select the correct meaning" : challenge.question;
@@ -87,7 +116,7 @@ export const Quiz = ({
         <Footer 
         disabled={!selectedOption}
         status={status}
-        onCheck={() =>{}}
+        onCheck={onContinue}
         />
 
         </>
