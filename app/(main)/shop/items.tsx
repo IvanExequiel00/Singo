c2c5e48ad2d5a75c3799ead57a1f9739e53bd519
IvanRojas FT47b <exequieliv@outlook.es>
2024-06-25 04:17:46 -0300

    shopp

---
+++ app/(main)/shop/items.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+type Props ={
+    hearts: number;
+    points: number;
+    hasActiveSubscription: boolean;
+}
+
+
+
+export const Items = ({
+    hearts,
+    points,
+    hasActiveSubscription
+}:Props) =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
