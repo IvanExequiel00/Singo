da52a095392c61add124e9921570fe219f514ac6
IvanRojas FT47b <exequieliv@outlook.es>
2024-06-25 05:11:08 -0300

    items

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useTransition } from "react";
+
 type Props ={
     hearts: number;
     points: number;
@@ -7,15 +11,52 @@ type Props ={
 }
 
 
+const POINTS_TO_REFILL = 10;
+
 
 export const Items = ({
     hearts,
     points,
     hasActiveSubscription
 }:Props) =>{
-    return(
-        <div>
+    const [pending, startTransition] = useTransition();
 
-        </div>
+
+    return(
+        <ul className="w-full">
+            <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
+                <Image 
+                src="/heart.svg"
+                alt="heart"
+                height={60}
+                width={60}
+                />
+                <div className="flex-1">
+                    <p className="text-neutral-700 text-base lg:text-xl font-bold"          >
+                        Refill hearts
+                    </p>
+
+                </div>
+                <Button 
+                disabled={hearts === 5 || points  < POINTS_TO_REFILL}
+                >
+                    {hearts === 5 ? "full" : (
+                        <div className="flex items-center">
+                            <Image 
+                            src="/points.svg"
+                            alt="Points"
+                            width={20}
+                            height={20}
+                            />
+                            <p>
+                                {POINTS_TO_REFILL}
+                            </p>
+
+                        </div>
+                    )}
+                </Button>
+            </div>
+
+        </ul>
     )
 }
