611688351fc7a82a585f7853f620120509e694e0
IvanRojas FT47b <exequieliv@outlook.es>
2024-06-26 02:51:22 -0300

    shop

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,7 +1,7 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import { getUserProgress, getUserSubscription } from "@/db/queries";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { Items } from "./items";
@@ -10,15 +10,20 @@ import { Items } from "./items";
 const shopPage = async () =>{
 
     const userProgressData = getUserProgress();
+    const userSubsciptionData = getUserSubscription();
     const [
-        userProgress
+        userProgress,
+        userSubscription
     ] = await Promise.all([
         userProgressData,
+        userSubsciptionData
     ]);
 
     if(!userProgress || !userProgress.activeCourse){
         redirect("/courses")
     }
+
+    const isPro = !!userSubscription?.isActive
 
 return(
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -27,7 +32,7 @@ return(
             activeCourse={userProgress.activeCourse}
             hearts={userProgress.hearts}
             points={userProgress.points}
-            hasActiveSubscription={false}
+            hasActiveSubscription={isPro}
             />
         </StickyWrapper>
 
@@ -48,7 +53,7 @@ return(
                 <Items 
                 hearts={userProgress.hearts}
                 points={userProgress.points}
-                hasActiveSubscription={false}
+                hasActiveSubscription={isPro}
                 />
 
             </div>
